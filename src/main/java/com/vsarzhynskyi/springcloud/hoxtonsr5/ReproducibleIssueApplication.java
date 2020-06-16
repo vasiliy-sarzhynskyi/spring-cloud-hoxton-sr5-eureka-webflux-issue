e7db89b32c6d3dec7333a788de03ba0e761709e5
vsarzhynskyi <vsarzhynskyi@playtika.com>
2020-06-16 21:28:33 +0300

    initial commit with reproducing  issue

---
+++ src/main/java/com/vsarzhynskyi/springcloud/hoxtonsr5/ReproducibleIssueApplication.java
@@ -0,0 +1,13 @@
+package com.vsarzhynskyi.springcloud.hoxtonsr5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReproducibleIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReproducibleIssueApplication.class, args);
+	}
+
+}
